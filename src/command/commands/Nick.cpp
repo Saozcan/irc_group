60c4e32876eb341d3c09177f6c0a0c64ceab1914
saozcan <samimozcan@outlook.com>
2023-03-15 05:03:06 +0300

    pass nick and user commands added, architecture is okay. fix

--- src/command/commands/Nick.cpp
+++ src/command/commands/Nick.cpp
@@ -7,6 +7,10 @@ Nick::~Nick() {}
 
 void Nick::execute(const std::vector<std::string>& splitArgs,  std::pair<const int, NormalUser*>& user, std::map<int, NormalUser*> &_users, std::map<std::string, Channel*> &_channels, const std::string &pass) {
     //TODO: nick unique olacaksa kontrol edilecek.
+    if (!user.second->getPass()) {
+        send(user.first, "Please enter pass\n", strlen("Please enter pass\n"), 0);
+        return ;
+    }
     std::string argString;
     splitArgs[1].find('\n') != std::string::npos ? argString = splitArgs[1].substr(0, splitArgs[1].size() - 1) : argString = splitArgs[1];
     user.second->setNick(argString);
