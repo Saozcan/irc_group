5bfb92571ea809ab5fef6d144d3e1995686cf25a
esrabudakk <esra_b1644@outlook.com>
2023-03-20 22:34:18 +0300

    Mode::execute function is ok

--- src/command/commands/Mode.cpp
+++ src/command/commands/Mode.cpp
@@ -16,53 +16,44 @@ Mode::~Mode() {
  * */
 
 void Mode::execute(const std::vector<std::string>& splitArgs,  std::pair<const int, NormalUser*>& user, Server& server){
-    size_t found;
-    std::string channel_name;
-    std::string new_user;
-    std::vector<std::string>::const_iterator it = splitArgs.begin() + 1;
-    std::map<std::string , Channel*>::iterator it_channel;
+    std::string channel_name = Utility::strTrim(splitArgs[1]);
+    std::string targetUser = Utility::strTrim(splitArgs[3]);
     std::map<int , NormalUser*>::iterator it_user = server._users.begin();
-
-    std::string user_name = Utility::strTrim(*(it + 2));
-    found = (*it).find('#') != std::string::npos ? (*it).find('#') : (*it).find('&');
-    channel_name = Utility::strTrim(*it);
+    std::map<std::string , Channel*>::iterator it_channel = server._channels.find(channel_name);
     bool checkUser = false;
-    if(found != std::string::npos)
+    if(it_channel == server._channels.end())
     {
-        it_channel = server._channels.find(channel_name);
-        if (!it_channel->first.empty()){
-            for (; it_user != server._users.end() ; it_user++) {
-                if(user_name == (*it_user).second->getName()) {
-                    checkUser = true;
-                }
-            }
-            if(checkUser){
-                std::string mode_flag = *(it + 1);
-                if(!mode_flag.empty()){
-                    if(it_channel->second->checkOperators(user.second->getName())){
-                        if(mode_flag == "+o"){
-                            it_channel->second->addMode(user_name);
-                            std::cout << user_name <<": User added as an operator"<< std::endl;
-                        }
-                        else if(mode_flag == "-o"){
-                            it_channel->second->removeMode(user_name);
-                            std::cout << user_name << ": User removed as an operator" << std::endl;
-                        }
-                        else
-                            std::cout << "Invalid mode flag" << std::endl;
-                    }
-                    else
-                        std::cout << "Sorry you are not an operator" << std::endl;
-                }
-                else
-                    std::cout << "Enter mode flag for operation" << std::endl;
-            }
-            else
-                std::cout << "User cannot be found! " << std::endl;
+        std::cout << "There is no such channel" << std::endl;
+        return;
+    }
+    for (; it_user != server._users.end() ; it_user++) {
+        if(targetUser == (*it_user).second->getName()) {
+            checkUser = true;
         }
-        else
-            std::cout << "We don't have such a channel!\nHint: JOIN command create new channel" << std::endl;
+    }
+    if(!checkUser)
+    {
+        std::cout << "There is no such user" << std::endl;
+        return;
+    }
+    size_t flag = splitArgs[2].find('+') != std::string::npos ? splitArgs[2].find('+') : splitArgs[2].find('-');
+    if(flag == std::string::npos ){
+        std::cout << "Enter mode flag for operation" << std::endl;
+        return;
+    }
+    if(!it_channel->second->checkOperators(user.second->getName())){
+        std::cout << "You are not an operator" << std::endl;
+        return;
+    }
+    std::string mode_flag = splitArgs[2];
+    if(mode_flag == "+o"){
+        it_channel->second->addMode(targetUser);
+        std::cout << targetUser <<": User added as an operator"<< std::endl;
+    }
+    else if(mode_flag == "-o"){
+        it_channel->second->removeMode(targetUser);
+        std::cout << targetUser << ": User removed as an operator" << std::endl;
     }
     else
-            std::cout << "Please enter the channel name" << std::endl;
+        std::cout << "Invalid mode flag" << std::endl;
 }
